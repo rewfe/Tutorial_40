6741605ee6b085166549d60261ce7919fd420ecb
rewfe <qrewfe@yandex.ru>
2015-07-08 04:18:33 +0300

    Initial Commit

---
+++ Tutorial_40/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Tutorial_40
+//
+//  Created by Admin on 08.07.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Tutorial_40/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Tutorial_40
+//
+//  Created by Admin on 08.07.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
